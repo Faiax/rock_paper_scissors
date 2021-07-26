d7fb8c31b5a293ae2aba66f6da1ea3df182a6899
Faiax <68774895+Faiax@users.noreply.github.com>
2021-07-26 14:53:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def play():
 
     if user == computer:
         return "you and your computer both chosen {}. So it's a tie ".format(computer)
+
     if is_win(user, computer):
         return "you have chosen {} and the computer has chosen {}. You won!".format(user, computer)
     return " your have choose {} adn the computer has chosen {}. you lost :(".format(user, computer)
@@ -17,9 +18,9 @@ def play():
 def is_win(player, opponent):
     # return true is the player beats the opponent
     # winning conditions: r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player=='s' and opponent =='p') or (player == 'p' and opponent == 'r' ):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
     return False
 
 
-play()
+print(play())
